eb74f5f1bc76f69b783a9031d7a6c06e7412c5be
gorbachev-dmitrii <gorbachev.dim@gmail.com>
2021-02-16 00:35:42 +0300

    infoViewController implemented

--- MyHabits/Controller/InfoViewController.swift
+++ MyHabits/Controller/InfoViewController.swift
@@ -8,40 +8,78 @@
 import UIKit
 
 class InfoViewController: UIViewController {
-
+    
+    private let containerView = UIView()
+    private let scrollView = UIScrollView()
+    
     let titleLabel: UILabel = {
         let label = UILabel()
-        label.numberOfLines = 0
+        label.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
         label.text = "Привычка за 21 день"
         return label
     }()
     
     let descriptionLabel: UILabel = {
         let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 17, weight: .regular)
         label.numberOfLines = 0
         return label
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.addSubview(scrollView)
+        scrollView.addSubview(containerView)
+        containerView.addSubview(titleLabel)
+        containerView.addSubview(descriptionLabel)
+        disableAutoresizingMask(views: [scrollView, containerView, titleLabel, descriptionLabel])
+        setupConstraints()
+        loadDescription()
     }
     
-    
-    
-    
+    // MARK: Setup Constraints
     func setupConstraints() {
-        
+        NSLayoutConstraint.activate([
+            
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            
+            containerView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            containerView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            containerView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            containerView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            
+            titleLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 22),
+            titleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 16),
+            titleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -16),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 16),
+            descriptionLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            descriptionLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor),
+            descriptionLabel.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -22)
+            
+        ])
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func disableAutoresizingMask(views: [UIView]) {
+        views.forEach({
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        })
     }
-    */
-
+    
+    func loadDescription() {
+        if let filepath = Bundle.main.path(forResource: "Текст", ofType: "md") {
+            do {
+                let contents = try String(contentsOfFile: filepath)
+                descriptionLabel.text = contents
+            } catch {
+                print("cannot read from file")
+            }
+        } else {
+            print("cannot file the file ")
+        }
+    }
 }
