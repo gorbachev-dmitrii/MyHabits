326f6712e2b86f0335d64f9dc398af169aba9956
gorbachev-dmitrii <gorbachev.dim@gmail.com>
2021-02-22 20:46:00 +0300

    progress cell implemented, configuring cells refactored

--- MyHabits/Controller/HabitsViewController.swift
+++ MyHabits/Controller/HabitsViewController.swift
@@ -9,31 +9,45 @@ import UIKit
 
 class HabitsViewController: UIViewController {
 
-    let store = HabitsStore.shared
-    private let layout = UICollectionViewFlowLayout()
-    private lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+    var store = HabitsStore.shared
+
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collection.dataSource = self
+        collection.delegate = self
+        collection.register(
+            HabitCollectionViewCell.self,
+                    forCellWithReuseIdentifier: String(describing: HabitCollectionViewCell.self))
+        collection.register(
+            ProgressCollectionViewCell.self,
+                    forCellWithReuseIdentifier: String(describing: ProgressCollectionViewCell.self))
+        return collection
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(presentModally))
         navigationItem.rightBarButtonItem?.tintColor = UIColor(named: "myPurple")
         title = "Cегодня"
-        collectionView.dataSource = self
-        collectionView.delegate = self
-        collectionView.register(
-            HabitCollectionViewCell.self,
-                    forCellWithReuseIdentifier: String(describing: HabitCollectionViewCell.self))
+       
         view.addSubview(collectionView)
         setupConstraints()
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.backgroundColor = UIColor(named: "myWhite")
         view.backgroundColor = UIColor(named: "myWhite")
-        //store.habits.removeAll()
         
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        print("viewDidAppear")
+        //store.todayProgress
     }
     
     @objc func presentModally() {
@@ -48,22 +62,11 @@ class HabitsViewController: UIViewController {
             collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 22),
             collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
             collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
-            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
         ])
     }
-}
-
-extension HabitsViewController: UICollectionViewDataSource {
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return store.habits.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
+    // MARK: Set custom cells
+    func setHabitCell(indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: HabitCollectionViewCell.self), for: indexPath) as! HabitCollectionViewCell
         cell.backgroundColor = .white
         cell.layer.cornerRadius = 8
@@ -71,11 +74,38 @@ extension HabitsViewController: UICollectionViewDataSource {
         cell.habit = habit
         return cell
     }
+    
+    func setProgressCell(indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: ProgressCollectionViewCell.self), for: indexPath)
+        cell.layer.cornerRadius = 8
+        cell.backgroundColor = .white
+        return cell
+    }
 }
-
+// MARK: UICollectionViewDataSource
+extension HabitsViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return section > 0 ? store.habits.count : 1
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return indexPath.section > 0
+        ? setHabitCell(indexPath: indexPath)
+        : setProgressCell(indexPath: indexPath)
+    }
+}
+// MARK: UICollectionViewDelegateFlowLayout
 extension HabitsViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let width: CGFloat = collectionView.bounds.width
-        return CGSize(width: width, height: 130)
+        return indexPath.section > 0
+            ? CGSize(width: width, height: 130)
+            : CGSize(width: width, height: 60)
     }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+           return UIEdgeInsets(top: 0, left: 0, bottom: 18, right: 0)
+        }
 }
--- MyHabits/View/ProgressCollectionViewCell.swift
+++ MyHabits/View/ProgressCollectionViewCell.swift
@@ -8,5 +8,60 @@
 import UIKit
 
 class ProgressCollectionViewCell: UICollectionViewCell {
+    var store: HabitsStore? {
+        didSet {
+            if let pr = store?.todayProgress {
+                //progressView.progress = pr
+                print(progressView.progress)
+            } else {
+                //progressView.progress = 0
+            }
+        }
+    }
     
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.setFootnoteStatusFont()
+        label.text = "Все получится"
+        return label
+    }()
+    
+    let progressView: UIProgressView = {
+        let progress = UIProgressView()
+        progress.progressViewStyle = .bar
+        progress.progressTintColor = UIColor(named: "myPurple")
+        progress.backgroundColor = .systemGray2
+        progress.clipsToBounds = true
+        return progress
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubviews(views: [titleLabel, progressView])
+        contentView.disableAutoresizingMask(views: [titleLabel, progressView])
+        setupConstraints()
+        progressView.progress = 0.5
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        progressView.layer.cornerRadius = 4
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12),
+            
+            progressView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
+            progressView.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            progressView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -12),
+            progressView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -15),
+            progressView.heightAnchor.constraint(equalToConstant: 7)
+        ])
+    }
 }
